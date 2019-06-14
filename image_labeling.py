0a2bacbc061bcee14b3222b64dc925d1de438872
hzkyah <44174716+hzkyah@users.noreply.github.com>
2019-06-14 14:29:37 -0700

    Update image_labeling.py

--- image_labeling.py
+++ image_labeling.py
@@ -18,24 +18,27 @@
 # [0, 0, 0, 0, 0, 3, 0],
 # [0, 0, 0, 0, 0, 3, 0]]
 
-def neighboors(matrix, i, j):
+import itertools
+
+def neighboors(matrix, row, col):
     length = len(matrix)
     width = len(matrix[0]) 
-    if i < length - 1 and j < width - 1:
-        return [(i, j+1), (i+1, j), (i+1, j+1)]
-    if i == length - 1 and j < width - 1:
-        return [(i, j+1)]
-    if j == width - 1 and i < length - 1:
-        return [(i+1, j)]
-    return []
+    left = col - 1 if col - 1 >= 0 else col
+    right = col + 1 if col + 1 < width else col
+    top = row - 1 if row - 1 >= 0 else row
+    bottom = row + 1 if row + 1 < length else row
+    coordinates = itertools.product([row, top, bottom], [col, left, right])
+    coordinates = set(coordinates)
 
-def shade_neighbours(matrix, i, j, shade_value):
-    peers = neighboors(matrix, i, j)
+    return coordinates
+
+def shade_neighbours(matrix, row, col, shade_value):
+    peers = neighboors(matrix, row, col)
+    matrix[row][col] = shade_value
     if len(peers) > 0:
         for coord in peers:
             if matrix[coord[0]][coord[1]] == 1:
                 shade_neighbours(matrix, coord[0], coord[1], shade_value)
-    matrix[i][j] = shade_value
     
 def matrix_function(matrix):
     length = len(matrix)
@@ -47,3 +50,16 @@ def matrix_function(matrix):
                 next_block += 1
                 shade_neighbours(matrix, i, j, next_block)
     return matrix
+
+m = [[0, 1, 0, 0, 0, 0, 1],
+    [0, 1, 1, 1, 1, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1, 0]]
+
+result = matrix_function(m)
+for i in range(len(result)):
+    for j in range(len(result[0])):
+        print(result[i][j], end='')
+    print()
